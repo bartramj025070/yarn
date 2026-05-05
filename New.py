43438d9405cc21e0fd49239413d1a37c8d318732
Jasper Bartram <134063226+bartramj025070@users.noreply.github.com>
2026-05-05 09:55:35 +0100

    vro i hate stackoverflo :c
    Removed broken function and added the CheckForOAuth() function to the Awake procedure.

--- New.py
+++ New.py
@@ -36,39 +36,20 @@ on the computers.
 """
 
 OAuthName = "SSH Key"
-OAuthDir = os.path.join(os.path.basename(__file__), "OAuth.hid")
-
-# https://stackoverflow.com/questions/9202224/getting-a-hidden-password-input
-def secure_password_input(prompt=''):
-    p_s = ''
-    proxy_string = [' '] * 64
-    while True:
-        sys.stdout.write('\x0D' + prompt + ''.join(proxy_string))
-        c = msvcrt.getch()
-        if c == b'\r':
-            break
-        elif c == b'\x08':
-            p_s = p_s[:-1]
-            proxy_string[len(p_s)] = " "
-        else:
-            proxy_string[len(p_s)] = "*"
-            p_s += c.decode()
-
-    sys.stdout.write('\n')
-    return p_s
+OAuthDir = os.path.join(os.path.dirname(__file__), "OAuth.hid")
 
 def CheckForOAuth():
-    hasOAuth = os.path.exists(
-        OAuthDir
-    )
+    hasOAuth = os.path.exists(OAuthDir)
     if not hasOAuth:
-        OAuth = secure_password_input("Please enter your " + OAuthName + " >>  ")
+        OAuth = input("Please enter your " + OAuthName + " >>  ")
         with open(OAuthDir, 'w') as file:
             file.write(OAuth)
         os.system("cls")
+        print(HEADER)
 
 def Awake():
     print(HEADER)
+    CheckForOAuth()
     projectName = "Enter your project name >>  "
     
 Awake()
