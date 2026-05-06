79fa3cef4429a64a43fbedff66e89de5916f1406
Jasper Bartram <134063226+bartramj025070@users.noreply.github.com>
2026-05-06 12:07:53 +0100

    updayted new to create properly
    look into at home - at school makes it difficult to work with... look into either using the REST API or keeping with the PAT method and using the os module for system commands -- REST API could be better, but https may be blocked ?

--- New.py
+++ New.py
@@ -26,7 +26,7 @@ tokens, or for where you don't have access to the
 git shell.
 
 This file creates a new Yarn project, follow it as such
-and use run the Push and Pull files respectively in the
+and run the Push and Pull files respectively in the
 Yarn project.
 
 The entire project is written in Python, for ease-of-use,
@@ -38,37 +38,63 @@ on the computers.
 OAuthName = "PAT (Personal Access Token)"
 
 filePath = os.path.dirname(__file__)
-OAuthDir = os.path.join(filePath, "OAuth.csv")
+OAuthDir = os.path.join(filePath, ".pat.csv")
 gitIgnoreDir = os.path.join(filePath, ".gitignore")
 
+# Filled in CheckForOAuth()
+OAuth = {
+    "username": "mrrp :3",
+    "token": "mrrp :3",
+}
+
 def CheckForOAuth():
+    global OAuth
     hasOAuth = os.path.exists(OAuthDir) and os.path.exists(gitIgnoreDir)
+    
     if not hasOAuth:
         with open(gitIgnoreDir, 'w') as gitIgnore:
             gitIgnore.writelines([
-                'projects',
+                'projects\n',
                 os.path.basename(OAuthDir)
             ])
         
         githubUsername = input("Please enter your GitHub username >>  ")
+        
         print("\nIf you don't have a PAT, go to: https://github.com/settings/personal-access-tokens")
-        OAuth = input("Please enter your " + OAuthName + " >>  ")
+        PAToken = input("Please enter your " + OAuthName + " >>  ")
+        
         with open(OAuthDir, 'w') as file:
-            file.write(f"{githubUsername},{OAuth}")
+            file.write(f"{githubUsername},{PAToken}")
+        
         os.system("cls")
         print(HEADER)
+    
+    with open(OAuthDir, 'r') as file:
+        contents = file.read().split(',')
+        
+        username = contents[0]
+        token = contents[1]
+        
+        OAuth["username"] = username
+        OAuth["token"] = token
 
 def Awake():
     print(HEADER)
     CheckForOAuth()
     
     projectName = input("Enter your project name >>  ")
-    projectDir = os.path.join(filePath, projectName)
+    
+    projectsDir = os.path.join(filePath, "projects")
+    projectDir = os.path.join(projectsDir, projectName)
+    
+    if not os.path.exists(projectsDir):
+        os.mkdir(projectsDir)
     
     if os.path.exists(projectDir):
         print("This project already exists!")
         input()
         return
     os.mkdir(projectDir)
+    
     
 Awake()
